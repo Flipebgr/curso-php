7aab9c4123aadc603ed282f019bd3d618c629f71
Filipe Menezes <Flipebgmufp@gmail.com>
2026-01-30 14:08:49 -0300

    Exerício 003 - tipos primitivos

---
+++ ex003/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="Pt-br">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=devide-width, initial-scale=1.0">
+   <title>Tipos primitivos</title>
+</head>
+     <body>
+        <h1>Estudo tipos primitivos</h1>
+        <?php 
+            $valorElevado = 13e2;
+            const TIPO_VARIAVEL_DOUBLE = "double";
+            echo "O valor de 13 * 10² é = $valorElevado<br>Essa variável é do tipo " . TIPO_VARIAVEL_DOUBLE ."<br>";
+            
+            $valorBoolean = true;
+            var_dump($valorBoolean);
+     
+        ?>     
+
+    </body>
+</html>
